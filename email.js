76afe1ed53490ba4614b0ab1bb4c0c2ecdc58f0e
Rakib-76 <mrrakibhasan76@gmail.com>
2025-02-19 01:19:41 +0600

    added the modified function of email

--- email.js
+++ email.js
@@ -1,4 +1,7 @@
 function  validEmail( email ) {
+    if (typeof email !== "string"){
+        return "Invalid";
+    }
     if (email.startsWith( "+") || email.startsWith("-") || email.startsWith("_") || email.startsWith("@") || email.startsWith(".")){
         return false;
     }
@@ -15,12 +18,15 @@ function  validEmail( email ) {
     }
     
 }
-const result1 = validEmail('rakib97@gmail.net');
-const result2 = validEmail('-rakib97@gmail.com');
-const result3 = validEmail('+rakib97@gmail.com');
-const result4 = validEmail('@rakib97@gmail.com');
-const result5 = validEmail('rakib97@gmail');
-const result6 = validEmail('ha san97@gmail.com');
-const result7 = validEmail('rakib97@gmail.com');
-console.log(result1 , result2, result3, result4 ,result5 ,result6,result7 );
+// const result1 = validEmail('rakib97@gmail.net');
+// const result2 = validEmail('-rakib97@gmail.com');
+// const result3 = validEmail('+rakib97@gmail.com');
+// const result4 = validEmail('@rakib97@gmail.com');
+// const result5 = validEmail('rakib97@gmail');
+// const result6 = validEmail('ha san97@gmail.com');
+// const result7 = validEmail('rakib97@gmail.com');
+// console.log(result1 , result2, result3, result4 ,result5 ,result6,result7 );
 
+// const result = validEmail("rakibhasan97@gmail.com");
+const result = validEmail(32);
+console.log(result);
